5bd809d8019eb906761eba306d79ac98bf49fc5c
Rone <rone.castro@lnls.br>
2021-04-24 14:38:21 -0300

    actions between account logins

--- client_venv/app/forms.py
+++ client_venv/app/forms.py
@@ -6,7 +6,7 @@ from app.models import User
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()], render_kw={'autofocus': True})
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
+    #remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
 class RegistrationForm(FlaskForm):
