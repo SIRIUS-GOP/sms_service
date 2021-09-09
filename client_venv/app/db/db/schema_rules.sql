108a13d52cb30d8c99dba389cfcdbeda47aed37a
Rone <rone.castro@lnls.br>
2021-09-09 08:56:50 -0300

    update files

--- client_venv/app/db/db/schema_rules.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS rules_db;
-
-CREATE TABLE rules_db (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    rule TEXT NOT NULL,
-    description TEXT NOT NULL,
-    owner TEXT NOT NULL
-);
