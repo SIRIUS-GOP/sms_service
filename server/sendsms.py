430060d6c85314632c469ad452d0c5a31f69828f
Rone <rone.castro@lnls.br>
2024-06-01 14:41:05 -0300

    sms sending function update, new feature, force sending, try 4x

--- server/sendsms.py
+++ server/sendsms.py
@@ -1,11 +1,20 @@
 import serial
 import serial.tools.list_ports
+import random
+import string
+import copy
 from time import sleep
+from datetime import datetime as dt
 
 class TextMessage:
-    def __init__(self, recipient="", message=""):
+    def __init__(self, recipient="", message="", debug=False):
         self.recipient = recipient
         self.content = message
+        self.debug = debug
+        self.smsnumber = None
+        self.sent_dt = None
+        self.ser = None
+        self.pipeline = "OK"
 
     pipeline = "OK"
     ans = b''
@@ -25,7 +34,8 @@ class TextMessage:
     def connectPhone(self):
         try:
             modem_port = self.get_port()
-            print('modem_port', modem_port)
+            if self.debug:
+                print('modem_port', modem_port)
             self.ser = serial.Serial(modem_port, 115200, timeout=2)
             sleep(1)
             return 1
@@ -50,123 +60,278 @@ class TextMessage:
             pass
 
     def sComm(self, cmd, rsize=0):
-        print('cmd:', cmd)
+        if self.debug:
+            print('cmd:', cmd)
         try:
-            r = self.ser.write((cmd + '\r').encode())
+            r = self.ser.write(cmd.encode())
             if r:
-                sleep(1)
+                sleep(0.5)
                 if rsize > 0:
                     r = self.ser.read(rsize)
                     self.ans = r
                     if len(r) > 0:
                         r = r.decode("utf-8")
-                        print('Modem answer:', r)
-                        if (r.find(cmd)==-1 and r.find('\nOK\r')==-1):
+                        if self.debug:
+                            print('Modem answer:', r)
+                        if cmd == 'AT+CMGL="all"\r':
+                            return r
+                        find_cmd = r.find(cmd)
+                        find_error = r.find('ERROR')
+                        find_ok = r.find('\nOK\r')
+                        if find_error!=-1:
                             self.pipeline = cmd
-                            return 1
+                            return 0
+                        if (find_cmd==-1 and find_ok==-1):
+                            self.pipeline = cmd
+                            return 0
                         else:
                             self.pipeline = 'OK'
-                            return 0
+                            return 1
                     else:
                         pass
-                        # print('len(r) < 0', r)
                 else:
                     pass
-                    # print('error on write to modem')
-        except:
+        except Exception as e:
+            if self.debug:
+                print(e)
             self.pipeline = cmd
             return 1
 
+    def configureModem(self):
+        CNMI = 'AT+CNMI=2,1,0,1,0'
+        CSMP = 'AT+CSMP=49,167,0,0'
+        enter = '\r'
+        self.pipeline = 'OK'
+        if self.pipeline=='OK':
+            if self.clearmemory():
+                self.pipeline='OK'
+            else:
+                return 0
+        if self.pipeline=='OK':
+            self.sComm(CNMI + enter, 32)
+        else:
+            return self.pipeline
+        if self.pipeline=='OK':
+            self.sComm(CSMP + enter, 32)
+        else:
+            return self.pipeline
+        
+        
     def sendMessage(self):
         ATZ = 'ATZ'
         AT = 'AT'
         CMGF = 'AT+CMGF=1'
         CMGW = 'AT+CMGW=' + '"' + self.recipient + '"'
+        CMGS = 'AT+CMGS=' + '"' + self.recipient + '"'
         MSG = self.content
         CMSS = 'AT+CMSS='
         CMGD = 'AT+CMGD=1,4'
         content = self.content
         endContent = chr(26)
+        enter = '\r'
+        CNMI = 'AT+CNMI=2,1,0,1,0'
+        CSMP = 'AT+CSMP=49,167,0,0'
 
+        var = self.pipeline
+        
         if self.pipeline=='OK':
-            self.sComm(ATZ, 16)
+            self.sComm(ATZ + enter, 16)
         else:
             return self.pipeline
         if self.pipeline=='OK':
-            self.sComm(ATZ, 16)
+            self.sComm(ATZ + enter, 16)
         else:
             return self.pipeline
         if self.pipeline=='OK':
-            self.sComm(AT, 16)
+            self.sComm(AT + enter, 16)
+        else:
+            return self.pipeline        
+        if self.pipeline=='OK':
+            self.sComm(CMGF + enter, 16)
+        else:
+            return self.pipeline
+        # if self.pipeline=='OK':
+        #     self.sComm(CMGW, 32)
+        if self.pipeline=='OK':
+            self.sComm(CMGS + enter, 32)
         else:
             return self.pipeline
         if self.pipeline=='OK':
-            self.sComm(CMGF, 16)
+            sleep(1)
+            self.sComm(content[0:159], 160)
         else:
             return self.pipeline
         if self.pipeline=='OK':
-            self.sComm(CMGW, 32)
+            sleep(1)
+            self.sComm(endContent, 320)
+        else:
+            return self.pipeline
+        # if self.pipeline=='OK':
+        #     i = self.memSlot(self.ans.decode("utf-8"))
+        #     if i.isnumeric() and self.pipeline=='OK':
+        #         self.sComm((CMSS + i), 32)
+        #         sleep(2)
+        # else:
+        #     return self.pipeline
+        # if self.pipeline=='OK':
+        #     self.sent_dt = dt.now()
+        #     self.sComm(CMGD, 32)
+        # else:
+        #     return self.pipeline
+        if self.pipeline=='OK':
+            self.sent_dt = dt.now()
+            self.sComm(ATZ + enter, 16)
         else:
             return self.pipeline
         if self.pipeline=='OK':
-            self.sComm(content)
-        else:
-            return self.pipeline
-        if self.pipeline=='OK':
-            self.sComm(endContent, 256)
-        else:
-            return self.pipeline
-        if self.pipeline=='OK':
-            i = self.memSlot(self.ans.decode("utf-8"))
-            if i.isnumeric() and self.pipeline=='OK':
-                self.sComm((CMSS + i), 32)
-        else:
-            return self.pipeline
-        if self.pipeline=='OK':
-            self.sComm(CMGD, 32)
-        else:
-            return self.pipeline
-        if self.pipeline=='OK':
-            self.sComm(ATZ, 16)
-        else:
-            return self.pipeline
-        if self.pipeline=='OK':
-            self.sComm(ATZ, 16)
+            self.sComm(ATZ + enter, 16)
         else:
             return self.pipeline
 
         # self.pipeline='OK'
-
-        if self.pipeline == 'OK':
+        pipeline_result = self.pipeline
+        if pipeline_result == 'OK':
             return 1
         else:
             return 0
+
 
     def disconnectPhone(self):
         try:
             self.ser.close()
             return 1
         except:
-            return 'Error disconnecting modem'
+            return 0
 
-def sendSMS(phone, msg):
-    sms = TextMessage(phone, msg)
-    r = sms.connectPhone()
-    if r==True:
-        r = sms.sendMessage()
-        if r==True:
-            r = sms.disconnectPhone()
-            if r==True:
-                return r
+    def clearmemory(self):
+        CMGD = 'AT+CMGD=1,4'
+        enter = '\r'
+        if self.sComm(CMGD + enter, 16):
+            return 1
         else:
-            return r
-    else:
-        return r
+            return 0
+        
 
-# msg = "WARNING\n\rCAX:3:BASLER01:ImgDVFStatus-Mon = 0\n\rLimit: L=100\n\rRule: pv < L"
+    def set_delivery_report(self, report=True):
+        if report:
+            cmd = "AT+CNMI=" + '2,1,0,1,0' # values to receive report
+            enter = '\r'
+            ans = self.sComm(cmd + enter, 24)
+            # ans = self.get_answer(sleep=0.2)
+            if ans:
+                cmd = "AT+CSMP=" + '49,167,0,0' # values to receive report
+                ans = self.sComm(cmd + enter, 24)
+                # ans = self.get_answer(sleep=0.2)
+            else:
+                return ans
+        else:
+            cmd = "AT+CNMI=" + '2,1,2,1,0' # original values
+            ans = self.sComm(cmd + enter, 24)
+            # ans = self.get_answer(sleep=0.2)
+            if ans:
+                cmd = "AT+CSMP=" + '0,173,0,0' # original values
+                ans = self.sComm(cmd + enter, 24)
+                # ans = self.get_answer(sleep=0.2)
+            else:
+                return ans
+
+    def get_delivery_report(self, phonenumber, sent, delay, exclude_sms=True):
+        sleep(delay)
+        cmd = 'AT+CMGL="all"'
+        enter = '\r'
+        ans = self.sComm(cmd + enter, 320)
+        if ('AT+CMGL="all"' and 'OK' and not "+CMGL: " in ans):
+            return False
+        else:
+            ans = ans.split("+CMGL: ")
+            ans.remove(ans[0])
+            ans[-1] = ans[-1].split('OK')[0]
+            for elem in ans:
+                rec_number = elem.split('"",')[1].split('Torpedo SMS entregue p/ ')[1].split(' (')[0].strip()
+                sms_id = (elem.split(',"REC ')[0])
+                delivery_date = dt.strptime((elem.split('"","')[1]).split('-')[0], '%Y/%m/%d %H:%M:%S')
+                if (rec_number.strip()) in (phonenumber.strip()):
+                    if ((abs(delivery_date - sent).seconds) <= delay):
+                        if exclude_sms:
+                            cmd = "AT+CMGD=" + str(sms_id)
+                            ans = self.sComm(cmd + enter, 16)
+                            if ans:
+                                return True
+                            else:
+                                return False
+                        return True
+            return False
+
+    def randomword(self, length):
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(length))
+    
+    # Force delivery if Carrier denies it due Spam filter
+    def force_delivery(self):
+        is_delivered = False
+        i = 0
+        original_msg = copy.deepcopy(self.content)
+        randomword = self.randomword(10)
+        number = self.recipient
+        while is_delivered != True :
+            if i == 0:
+                if len(self.content + "\r\n" + randomword) >= 160:
+                    extra_len = abs(len(self.content + "\r\n" + randomword) - 160) 
+                    msg = copy.deepcopy(self.content[:(len(self.content) - extra_len)])
+                    msg = msg + "\r\n" + randomword
+                else:
+                    msg = copy.deepcopy(self.content + "\r\n" + randomword)
+                self.content = copy.deepcopy(msg)
+                sent = self.sendMessage()
+                if sent == True:
+                    is_delivered = self.get_delivery_report(number, self.sent_dt, 6)
+            else:
+                if i >= 3:
+                    break
+                randomword += self.randomword(5)
+                if len(original_msg + "\r\n" + randomword) >= 160:
+                    extra_len = abs(len(original_msg + "\r\n" + randomword) - 160)
+                    msg = copy.deepcopy(original_msg[:(len(original_msg) - extra_len)])
+                    msg = msg + '\r\n' + randomword
+                else:
+                    msg = original_msg + '\r\n' + randomword
+                self.content = copy.deepcopy(msg)
+                sent = self.sendMessage()
+                if sent == True:
+                    is_delivered = self.get_delivery_report(number, self.sent_dt, 6)
+            i += 1
+        return is_delivered
+
+
+def sendSMS(phone, msg, debug=False):
+    sms = TextMessage(phone, msg , debug=debug)
+    r = 0
+    if sms.connectPhone() == True:
+        sms.configureModem()
+        sms.smsnumber = phone
+        if sms.sendMessage() == True:
+            sent = sms.sent_dt
+            is_delivered = sms.get_delivery_report(phone, sent, 6)
+            if not(is_delivered):
+                if sms.force_delivery() == True:
+                    return sms.disconnectPhone()
+            else:
+                return sms.disconnectPhone()
+    return r
+
+
+
+# msg = "WARNING\n\rCAX:B:BASLER01:ImgDVFStatus-Mon = 0\n\rLimit: L=100\n\rRule: pv < L"
 # msg = "WARNING\n\r1) ^RAD:Thermo.+:TotalDoseRate:Dose$ = pv ok\n\rLimit: L=1\n\rRule: pv > L\n\r2) RAD:ELSE:TotalDoseRate:Dose = 1.04225251"
-# msg = "WARNING\n\rSI-13C4:DI-DCCT:Current-Mon = 99.918693512\n\rLimit: L=200\n\rRule: pv < L"
+# msg = "1) SI-13C4:DI-DCCT:Current-Mon = 99.918693512\n\rLimit: L=200\n\rRule: pv < L\n\r2) AS-Glob:AP-MachShift:Mode-Sts = 0\n\rLimit: L=0"
+# msg = "1) SI-13C4:DI-DCCT:Current-Mon = 100.00495162\n\rLimit: L=200\n\r2) AS-Glob:AP-MachShift:Mode-Sts = 0\n\rLimit: L=0"
+# msg = "1) SI-13C4:DI-DCCT:Current-Mon = 100.10985548\n\rLimit: L=420\n\r2) AS-Glob:AP-MachShift:Mode-Sts = 0\n\rLimit: L=69"
+# msg = "Here we go"
 # n = 124
 # msg = (msg[:n]) if len(msg) > n else msg
 # print(msg)
-# print(sendSMS('19997397443', msg))
+# phone = '19997397443'
+# if (sendSMS(phone, msg, debug=True)):
+#     print('Delivery successfull!')
+# else:
+#     print("Message not delivered!")
--- server/server.py
+++ server/server.py
@@ -7,41 +7,44 @@ from multiprocessing import Process, Queue, Pool, Manager, Value
 from time import sleep
 from datetime import datetime
 from re import sub
-from ctypes import c_bool, windll
+from ctypes import c_bool, windll, c_bool
 from systray import systray_run
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
+
 def on_new_client(connection, client_address, queue):
-    while True:
-        try:
-            #print('connection from', client_address)
-            ip1 = config.read("IP", "client_ip1") #changed to get connections only from optiplex-sc-02
-            ip2 = config.read("IP", "client_ip2")
-            ip3 = config.read("IP", "client_ip3")
-            ip4 = config.read("IP", "client_ip4")
-            data = ''
-            if (client_address[0] == ip1 or client_address[0] == ip2 or client_address[0] == ip3 or client_address[0] == ip4):
-                #Receive the data in small chunks and retransmit it
-                data = connection.recv(1024)
-                # print(datetime.now(),' received "%s"' % data, type(data))
-                if data:
-                    #print('sending data back to the client')
-                    connection.sendall(data) #echo
-                    to_sms = data.decode()
-                    #print('to_sms: ', to_sms, type(to_sms))
-                    queue.put_nowait(json.loads(to_sms)) #increment queue
-                else:
-                    #print('no data from ', client_address)
-                    break
+    # while True:
+    try:
+        #print('connection from', client_address)
+        ip1 = config.read("IP", "client_ip1") #changed to get connections only from optiplex-sc-02
+        ip2 = config.read("IP", "client_ip2")
+        ip3 = config.read("IP", "client_ip3")
+        ip4 = config.read("IP", "client_ip4")
+        data = ''
+        if (client_address[0] == ip1 or client_address[0] == ip2 or client_address[0] == ip3 or client_address[0] == ip4):
+            #Receive the data in small chunks and retransmit it
+            data = connection.recv(1024)
+            # print(datetime.now(),' received "%s"' % data, type(data))
+            if data:
+                #print('sending data back to the client')
+                connection.sendall(data) #echo
+                to_sms = data.decode()
+                #print('to_sms: ', to_sms, type(to_sms))
+                queue.put_nowait(json.loads(to_sms)) #increment queue
             else:
-                connection.close()
-        finally:
-            #Clean up the connection
+                #print('no data from ', client_address)
+                # break
+                pass
+        else:
             connection.close()
-            break
-    #print('on_new_client OFF')
-    quit()
+    finally:
+        #Clean up the connection
+        connection.close()
+        # break
+        print('on_new_client OFF')
+        # quit()
 
 def init():
     # Create a TCP/IP socket
@@ -79,7 +82,7 @@ def server(sock, queue, stop):
         #if client_address[0] != '10.20.31.19':
         try:
             #print('setting p')
-            p = Process(target=on_new_client, args=(connection, client_address, queue))
+            p = Process(target=on_new_client, args=(connection, client_address, queue), name='NSONC')
             print('on_new_client on')
             p.start()
         finally:
@@ -106,7 +109,7 @@ def watcherseye(queue, stop): #queue watcher
                 msg = 'ERROR on monitor.py'
                 pass
             elif int(n['numpvs']) == 1:
-                msg = prefix + '\n\r' + n['rule1']
+                msg = n['rule1'] # before it was: prefix + '\n\r' + n['rule1']
                 msg = msg.replace('==', '=', 1)
                 msg = msg.replace('!=', '=', 1)
                 msg = msg.replace('>=', '=', 1)
@@ -115,16 +118,18 @@ def watcherseye(queue, stop): #queue watcher
                 msg = msg.replace('<', '=', 1)
                 msg = msg.replace('L', n['value1'], 1)
                 msg = msg.replace('pv', n['pv1'], 1)
-                msg = msg + '\n\r' + 'Limit: ' + n['limits1'] + '\n\rRule: ' + n['rule1']
+                msg = msg + '\n\r' + 'Limit: ' + n['limits1']# + '\n\rRule: ' + n['rule1']
             elif int(n['numpvs']) == 2:
                 if (n['rule1'].find('(pv < LL) or (pv > LU)') != -1): #outside range
-                    msg1 = prefix + '\n\r1) ' + n['pv1'] + ' = ' + n['value1'] + '\n\r'+ 'Limits: ' + n['limits1'] + '\n\rRule: ' + n['rule1']
+                    msg1 = prefix + '\n\r1) ' + n['pv1'] + ' = ' + n['value1'] + '\n\r'+ 'Limits: ' + n['limits1']# + '\n\rRule: ' + n['rule1']
+                    # previous line: '1) ' + n['pv1'] + ' = ' + n['value1'] + '\n\r'+ 'Limits: ' + n['limits1']
                     # print('pv1 outside range')
                 elif (n['rule1'].find('(pv > LL) and (pv < LU)') != -1): #within range
-                    msg1 = prefix + '\n\r1) ' + n['pv1'] + ' = ' + n['value1'] + '\n\r'+ 'Limits: ' + n['limits1'] + '\n\rRule: ' + n['rule1']
+                    msg1 = prefix + '\n\r1) ' + n['pv1'] + ' = ' + n['value1'] + '\n\r'+ 'Limits: ' + n['limits1']# + '\n\rRule: ' + n['rule1']
+                    #previous line: '1) ' + n['pv1'] + ' = ' + n['value1'] + '\n\r'+ 'Limits: ' + n['limits1']# + '\n\rRule: ' + n['rule1']
                     # print('pv1 within range')
                 elif (bool(re.search('^pv .+ L$', n['rule1']))) != -1: #other rules
-                    msg1 = prefix + '\n\r1) ' + n['rule1']
+                    msg1 = '1) ' + n['rule1'] # previous version: prefix + '\n\r1) ' + n['rule1']
                     msg1 = msg1.replace('==', '=', 1)
                     msg1 = msg1.replace('!=', '=', 1)
                     msg1 = msg1.replace('>=', '=', 1)
@@ -133,12 +138,12 @@ def watcherseye(queue, stop): #queue watcher
                     msg1 = msg1.replace('<', '=', 1)
                     msg1 = msg1.replace('L', n['value1'], 1)
                     msg1 = msg1.replace('pv', n['pv1'], 1)
-                    msg1 = msg1 + '\n\r' + 'Limit: ' + n['limits1'] + '\n\rRule: ' + n['rule1']
+                    msg1 = msg1 + '\n\r' + 'Limit: ' + n['limits1']# + '\n\rRule: ' + n['rule1']
                     # print('pv1 pv=L choice')
                 if (n['rule2'].find('(pv < LL) or (pv > LU)') != -1): #outside range
-                    msg2 = '\n\r2) ' + n['pv2'] + ' = ' + n['value2'] + '\n\r'+ 'Limits: ' + n['limits2'] + '\n\rRule: ' + n['rule2']
+                    msg2 = '\n\r2) ' + n['pv2'] + ' = ' + n['value2'] + '\n\r'+ 'Limits: ' + n['limits2']# + '\n\rRule: ' + n['rule2']
                 elif (n['rule2'].find('(pv > LL) and (pv < LU)') != -1): #within range
-                    msg2 = '\n\r2) ' + n['pv2'] + ' = ' + n['value2'] + '\n\r'+ 'Limits: ' + n['limits2'] + '\n\rRule: ' + n['rule2']
+                    msg2 = '\n\r2) ' + n['pv2'] + ' = ' + n['value2'] + '\n\r'+ 'Limits: ' + n['limits2']# + '\n\rRule: ' + n['rule2']
                     # print('pv2 within range')
                 elif (bool(re.search('^pv .+ L$', n['rule2']))): #other rules
                     msg2 = '\n\r2) ' + n['rule2']
@@ -150,12 +155,12 @@ def watcherseye(queue, stop): #queue watcher
                     msg2 = msg2.replace('<', '=', 1)
                     msg2 = msg2.replace('L', n['value2'])
                     msg2 = msg2.replace('pv', n['pv2'])
-                    msg2 = msg2 + '\n\r' + 'Limit: ' + n['limits2'] + '\n\rRule: ' + n['rule2']
+                    msg2 = msg2 + '\n\r' + 'Limit: ' + n['limits2']# + '\n\rRule: ' + n['rule2']
                     # print('pv2 pv=L choice')
                 msg = msg1 + msg2
             elif int(n['numpvs']) == 3:
                 pass
-            num = 124
+            num = 144
             msg_sms = (msg[:num]) if len(msg) > num else msg
             # print("msg:\n\r", msg)
             # print("msg_sms:\n\r", msg_sms)
@@ -172,20 +177,22 @@ def watcherseye(queue, stop): #queue watcher
             
 def search(list, pname):
     pnum = 0
-    for i in range(len(list)):
-        if list[i] == pname:
-            pnum += 1
-    if pnum > 1:
-        return True
+    for elem in pname:
+        for i in range(len(list)):
+            if list[i] == elem:
+                pnum += 1
+        if pnum > 1:
+            return True
     return False
 
 def main():
-    proc = "pythonw.exe"
+    proc = ['NSServer', 'NSONC', 'NSWatcherseye', 'NSSystray', 'python.exe', 'pythonw.exe'] #"python.exe"
     processlist = []
+    
     for p in psutil.process_iter():
         try:
             processlist.append(p.name())
-            #print(p.name())
+            # print(p.name())
         except psutil.AccessDenied:
             #print("======= Access Denied ======= ")
             pass
@@ -204,13 +211,13 @@ def main():
         stop = Value(c_bool, False)
         exit = Value(c_bool, False)
         # t = systray_run(stop, exit)
-        p1 = Process(target=systray_run, args=(start, stop, exit))#
+        p1 = Process(target=systray_run, args=(start, stop, exit), name='NSSystray')#
         p1.start()
         # print('systray_run ok')
-        p2 = Process(target=server, args=(sock, q, stop))
+        p2 = Process(target=server, args=(sock, q, stop), name='NSServer')
         p2.start()
         # print('server ok')
-        p3 = Process(target=watcherseye, args=(q, stop))
+        p3 = Process(target=watcherseye, args=(q, stop), name='NSWatcherseye')
         p3.start()
         # print('watcherseye ok')
 #        while bool(exit.value) == False:
